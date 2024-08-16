4854d304ad3f2d5a5aca25554697c93f02d9bded
Jean Paul Ordonez <jean.ordnnez@uao.edu.co>
2024-08-16 12:58:14 -0500

    Tarea Functions Array

--- Tarea_Arreglos.js
+++ Tarea_Arreglos.js
@@ -1,2 +1,48 @@
-let numero = 1;
-console.log(numero);
+/*
+    Challenge: Funciones con arreglos
+    Autor: Jean Paul Ordoñez Ibarguen
+    Materia: Estructura de Datos 2
+    
+*/
+let resultado = ""
+let numero = parseInt(prompt("Ingrese el número: "))
+
+
+function funcionRegularAntigua(numero){
+    
+    if (numero % 2 === 0){
+        resultado = numero + " es par"
+    }else{
+        resultado = "El " + numero + " es impar"
+    }
+    console.log(resultado)
+    return resultado
+}
+funcionRegularAntigua(numero)
+
+
+const funcionRegularNueva = function(numero){
+
+    if (numero % 2 === 0){
+        resultado = numero + " es par"
+    }else{
+        resultado = "El " + numero + " es impar"
+    }
+    console.log(resultado)
+    return resultado
+
+}
+funcionRegularNueva(numero)
+
+
+const funcionFlecha = (numero) =>{
+
+    if (numero % 2 === 0){
+        resultado = numero + " es par"
+    }else{
+        resultado = "El " + numero + " es impar"
+    }
+    console.log(resultado)
+    return resultado
+}
+funcionFlecha(numero)
